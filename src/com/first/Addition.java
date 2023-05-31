e5541df0f3c15951bc48e506fbcccf96448635df
unknown <bestaumasree@gmail.com>
2023-05-31 17:47:09 +0530

    Addition

---
+++ src/com/first/Addition.java
@@ -0,0 +1,11 @@
+package com.first;
+
+public class Addition {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		int result = num1 + num2;
+		System.out.println("Addition of two numbers" + result);
+	}
+
+}
